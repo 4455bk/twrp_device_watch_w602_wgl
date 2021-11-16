37695eb5ff7fe9623b27103698cdc12fcace23cc
4455bk <rundsxe@gmail.com>
2021-11-16 19:04:01 +0800

    w602_wgl: Upload all relevant files

---
+++ omni_w602_wgl.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from w602_wgl device
+$(call inherit-product, device/watch/w602_wgl/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := w602_wgl
+PRODUCT_NAME := omni_w602_wgl
+PRODUCT_BRAND := w602
+PRODUCT_MODEL := Q1
+PRODUCT_MANUFACTURER := watch
+PRODUCT_RELEASE_NAME := w602 Q1
